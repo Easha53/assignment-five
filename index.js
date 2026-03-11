348354926c93ad7407e5f2a6546bdfe660577a5d
Easha53 <meherunnesaesha055@gmail.com>
2026-03-11 21:38:06 +0600

    4th commit

--- index.js
+++ index.js
@@ -7,3 +7,54 @@ for(let btn of Love){
     document.getElementById('logo-heart').innerText = Heart;
    })
 }
+
+// call buttons functions
+function callFunc(id){
+const value=20;
+   const Coin =parseInt(document.getElementById(id).innerText);
+   const UpdatedCoin = Coin - value;
+   document.getElementById(id).innerText = UpdatedCoin;
+
+   if(UpdatedCoin< 20)
+   {
+      alert("Less than 20 coins");
+   }
+
+}
+document.getElementById('b1').addEventListener('click', function(){
+   alert('National Emergency Number 999');
+   callFunc('coin-bar');
+})
+document.getElementById('b2').addEventListener('click', function(){
+   alert('Police Helpline Number 999');
+   callFunc('coin-bar');
+})
+document.getElementById('b3').addEventListener('click', function(){
+   alert('Fire Service Number 999');
+   callFunc('coin-bar');
+})
+document.getElementById('b4').addEventListener('click', function(){
+   alert('Ambulance Service 1994-999999');
+   callFunc('coin-bar');
+})
+document.getElementById('b5').addEventListener('click', function(){
+   alert('Women & Child Helpline 109');
+   callFunc('coin-bar');
+})
+document.getElementById('b6').addEventListener('click', function(){
+   alert('Anti-Corruption Helpline 106');
+   callFunc('coin-bar');
+})
+document.getElementById('b7').addEventListener('click', function(){
+   alert('Electricity Helpline 16216');
+   callFunc('coin-bar');
+})
+document.getElementById('b8').addEventListener('click', function(){
+   alert('Brac Helpline 16445');
+   callFunc('coin-bar');
+})
+document.getElementById('b9').addEventListener('click', function(){
+   alert('Bangladesh Railway Helpline 163');
+   callFunc('coin-bar');
+})
+
